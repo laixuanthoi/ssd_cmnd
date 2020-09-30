2cb5ee35d051b7418d6247b6f645f77119d9af36
laixuanthoi <laixuanthoi@gmail.com>
2020-09-30 15:15:59 +0700

    ok

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -76,20 +76,46 @@ def xml_to_csv(path):
         The produced dataframe
     """
 
+    # xml_list = []
+    # for xml_file in glob.glob(path + '/*.xml'):
+    #     tree = ET.parse(xml_file)
+    #     root = tree.getroot()
+    #     for member in root.findall('object'):
+    #         print(root.find('filename').text)
+    #         value = (root.find('filename').text,
+    #                  int(root.find('size')[0].text),
+    #                  int(root.find('size')[1].text),
+    #                  member[0].text,
+    #                  int(member[5][0].text),
+    #                  int(member[5][1].text),
+    #                  int(member[5][2].text),
+    #                  int(member[5][3].text)
+    #                  )
+    #         xml_list.append(value)
+    # column_name = ['filename', 'width', 'height',
+    #                'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    # xml_df = pd.DataFrame(xml_list, columns=column_name)
+    # return xml_df
     xml_list = []
     for xml_file in glob.glob(path + '/*.xml'):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
-            print(root.find('filename').text)
+            # bnd box contain 4 coordinate of format [xmin, ymin, xmax, ymax]
+            bndbox = member.find('bndbox')
+
+            # if object is None, ignore
+            if member.find('name') is None:
+                continue
+
             value = (root.find('filename').text,
                      int(root.find('size')[0].text),
                      int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[5][0].text),
-                     int(member[5][1].text),
-                     int(member[5][2].text),
-                     int(member[5][3].text)
+                     member.find('name').text,
+                     int(bndbox.find('xmin').text),
+                     int(bndbox.find('ymin').text),
+                     int(bndbox.find('xmax').text),
+                     int(bndbox.find('ymax').text)
                      )
             xml_list.append(value)
     column_name = ['filename', 'width', 'height',
